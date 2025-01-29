2b14702f9c68e640e62845c2c70ee8f59abf56ee
Summer <126639731+gaon5@users.noreply.github.com>
2025-01-29 17:58:00 +1300

    Update md5.py

--- md5.py
+++ md5.py
@@ -27,16 +27,6 @@ import base64
 #     pic_str = pic_b64.decode()
 
 
-"""
-步骤如下：【RSA一般都会结合base64编码一起使用的】
-第一步：读取公钥信息内容
-第二步：通过RSA导入公钥信息，并返回公钥对象
-第三步： 基于公钥创建RSA加密器对象
-第四步：通过RSA加密器对象进行加密（加密前数据转换为二进制格式）
-第五步：需要先进行base64编码，才可以转化为字符串可用文本
-第六步： 将二进制数据转化为文本
-
-"""
 import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
